0def8a2ac71b9be659e5ea86dc517583b2766d55
Kieran Doonan <kieran.doonan@savvi.co>
2022-07-19 13:51:38 +0100

    feat: add basic arrival / departure times to route

--- src/components/Route/index.jsx
+++ src/components/Route/index.jsx
@@ -4,10 +4,37 @@ import React from 'react'
 import { PropTypes } from 'prop-types'
 
 export default function Route ({ stops }) {
-  const details = stops.map(stop => {
+  const details = stops.map((stop, i) => {
     const passed = stop.skipped || stop.departure.actual
+
+    // Showing the time should follow these general rules
+    // - If a bus has departed, show that time
+    // - If it's the first stop, show the departure time
+    // - Following stops show the arrival time
+    // - If an estimated time is available, show that, otherwise show the scheduled time
+    // - If a stop was skipped, don't show any time
+    let timeShown
+    if (stop.departure.actual) {
+      const date = new Date(Date.parse(stop.departure.actual))
+      timeShown = `Departed ${date.toLocaleTimeString()}`
+    } else if (stop.skipped) {
+      timeShown = 'Skipped'
+    } else if (i === 0 && stop.departure.estimated) {
+      const date = new Date(Date.parse(stop.departure.estimated))
+      timeShown = `Departing ${date.toLocaleTimeString()}`
+    } else if (i === 0) {
+      const date = new Date(Date.parse(stop.departure.scheduled))
+      timeShown = `Departing ${date.toLocaleTimeString()}`
+    } else if (stop.arrival.estimated) {
+      const date = new Date(Date.parse(stop.arrival.estimated))
+      timeShown = `Arriving ${date.toLocaleTimeString()}`
+    } else {
+      const date = new Date(Date.parse(stop.arrival.scheduled))
+      timeShown = `Arriving ${date.toLocaleTimeString()}`
+    }
+
     return <li key={stop.location.detailed_name} className={`stop ${passed ? 'grey' : 'green'}`}>
-      { stop.location.detailed_name } { stop.departure.estimated }
+      <div className='stopName'>{ stop.location.detailed_name }</div><div className='stopTime'>{ timeShown }</div>
     </li>
   })
 
